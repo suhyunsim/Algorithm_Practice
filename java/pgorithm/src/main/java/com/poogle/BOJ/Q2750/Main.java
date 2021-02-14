277ef97c4b66e72969f074033c07418334408979
suhyunsim <cnc1106@naver.com>
2021-02-14 22:49:22 +0900

    boj: 2750 풀이
    Closed #12

---
+++ java/pgorithm/src/main/java/com/poogle/BOJ/Q2750/Main.java
@@ -0,0 +1,20 @@
+package main.java.com.poogle.BOJ.Q2750;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(Integer.parseInt(br.readLine()));
+        }
+        Collections.sort(list);
+        for (int i : list) System.out.println(i);
+    }
+}
