9e34ed1442e853e792c664ab6cdf9e2b02d6de55
suhyunsim <cnc1106@naver.com>
2021-03-11 14:38:59 +0900

    boj: 18258 풀이
    Closed #26

--- src/main/java/com/poogle/BOJ/Q18258/Main.java
+++ src/main/java/com/poogle/BOJ/Q18258/Main.java
@@ -5,7 +5,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Deque;
 import java.util.LinkedList;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -14,32 +13,30 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         StringTokenizer st;
         int n = Integer.parseInt(br.readLine());
-        Queue<Integer> queue = new LinkedList<>();
-        int last = 0;
+        Deque<Integer> deque = new LinkedList<>();
         while (n-- > 0) {
             st = new StringTokenizer(br.readLine(), " ");
             switch (st.nextToken()) {
                 case "push":
-                    last = Integer.parseInt(st.nextToken());
-                    queue.offer(last);
+                    deque.offer(Integer.parseInt(st.nextToken()));
                     break;
                 case "pop":
-                    if (!queue.isEmpty()) sb.append(queue.poll()).append('\n');
+                    if (!deque.isEmpty()) sb.append(deque.poll()).append('\n');
                     else sb.append(-1).append('\n');
                     break;
                 case "size":
-                    sb.append(queue.size()).append('\n');
+                    sb.append(deque.size()).append('\n');
                     break;
                 case "empty":
-                    if (!queue.isEmpty()) sb.append("0").append('\n');
+                    if (!deque.isEmpty()) sb.append("0").append('\n');
                     else sb.append(1).append('\n');
                     break;
                 case "front":
-                    if (!queue.isEmpty()) sb.append(queue.peek()).append('\n');
+                    if (!deque.isEmpty()) sb.append(deque.peek()).append('\n');
                     else sb.append(-1).append('\n');
                     break;
                 case "back":
-                    if (!queue.isEmpty()) sb.append(last).append('\n');
+                    if (!deque.isEmpty()) sb.append(deque.peekLast()).append('\n');
                     else sb.append(-1).append('\n');
                     break;
                 default:
