4404997f614609bd02d79bfa18c252aaf82b0e49
suhyunsim <cnc1106@naver.com>
2021-06-23 18:15:49 +0900

    PG: 12934 풀이

---
+++ src/main/java/com/poogle/PG/Q12934/Solution.java
@@ -0,0 +1,12 @@
+package main.java.com.poogle.PG.Q12934;
+
+public class Solution {
+    static long solution(long n) {
+        if (Math.pow((int) Math.sqrt(n), 2) == n) return (long) Math.pow(Math.sqrt(n) + 1, 2);
+        else return -1;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(121));
+    }
+}
