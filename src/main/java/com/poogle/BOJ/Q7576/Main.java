105485560c89b0d26a76d87cc39eee1d466e6f94
suhyunsim <cnc1106@naver.com>
2021-07-12 21:14:08 +0900

    BOJ: 7576 풀이
    Closed #122

--- src/main/java/com/poogle/BOJ/Q7576/Main.java
+++ src/main/java/com/poogle/BOJ/Q7576/Main.java
@@ -1,6 +1,5 @@
 package main.java.com.poogle.BOJ.Q7576;
 
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -15,35 +14,19 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
+        m = sc.nextInt();
+        n = sc.nextInt();
         board = new int[n][m];
         dist = new int[n][m];
+        Queue<Node> queue = new LinkedList<>();
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 board[i][j] = sc.nextInt();
-                dist[0][0] = 0;
-                if (board[i][j] == 1) bfs(i, j);
+                if (board[i][j] == 1) queue.add(new Node(i, j));
                 if (board[i][j] == 0) dist[i][j] = -1;
             }
         }
-        System.out.println(Arrays.deepToString(dist));
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (dist[i][j] == -1) {
-                    System.out.println(-1);
-                    return;
-                }
-                answer = Math.max(answer, dist[i][j]);
-            }
-        }
-        System.out.println(answer);
-    }
-
-    private static void bfs(int x, int y) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x, y));
         while (!queue.isEmpty()) {
             Node now = queue.poll();
             for (int i = 0; i < 4; i++) {
@@ -55,6 +38,17 @@ public class Main {
                 queue.offer(new Node(nx, ny));
             }
         }
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (dist[i][j] == -1) {
+                    System.out.println(-1);
+                    return;
+                }
+                answer = Math.max(answer, dist[i][j]);
+            }
+        }
+        System.out.println(answer);
     }
 }
 
