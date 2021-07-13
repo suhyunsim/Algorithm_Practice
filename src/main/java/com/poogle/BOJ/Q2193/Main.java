45ff78c2a836c0aacebb5704a13d55c1d96fa444
suhyunsim <cnc1106@naver.com>
2021-07-13 18:00:58 +0900

    BOJ: 2193 풀이
    Closed #129

---
+++ src/main/java/com/poogle/BOJ/Q2193/Main.java
@@ -0,0 +1,19 @@
+package main.java.com.poogle.BOJ.Q2193;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        long[] d = new long[n + 1];
+        d[1] = 1;
+        if (n >= 2) {
+            d[2] = 1;
+        }
+        for (int i = 3; i <= n; i++) {
+            d[i] = d[i - 1] + d[i - 2];
+        }
+        System.out.println(d[n]);
+    }
+}
