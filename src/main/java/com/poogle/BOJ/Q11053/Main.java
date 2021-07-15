e253ed1eb2ff961e7cb2de94ad496ea23931415e
suhyunsim <cnc1106@naver.com>
2021-07-15 22:31:24 +0900

    style: 공백 수정

--- src/main/java/com/poogle/BOJ/Q11053/Main.java
+++ src/main/java/com/poogle/BOJ/Q11053/Main.java
@@ -13,7 +13,7 @@ public class Main {
         int[] d = new int[n];
         for (int i = 0; i < n; i++) {
             d[i] = 1;
-            for (int j =0 ; j < i; j++) {
+            for (int j = 0; j < i; j++) {
                 if (a[j] < a[i] && d[i] < d[j] + 1) {
                     d[i] = d[j] + 1;
                 }
