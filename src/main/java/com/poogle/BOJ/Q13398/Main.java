12c247d84e6b3e1aa9abae7d10e40608e404b4d3
suhyunsim <cnc1106@naver.com>
2021-07-23 21:39:30 +0900

    BOJ: 13398 풀이
    Closed #144

---
+++ src/main/java/com/poogle/BOJ/Q13398/Main.java
@@ -0,0 +1,41 @@
+package main.java.com.poogle.BOJ.Q13398;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] a = new int[n];
+        int[] dL = new int[n];
+        int[] dR = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        for (int i = 0; i < n; i++) {
+            dL[i] = a[i];
+            if (i > 0 && dL[i] > dL[i - 1] + a[i]) {
+                dL[i] = dL[i - 1] + a[i];
+            }
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            dR[i] = a[i];
+            if (i < n - 1 && dR[i] < dR[i + 1] + a[i]) {
+                dR[i] = dR[i + 1] + a[i];
+            }
+        }
+        int ans = dL[0];
+        for (int i = 1; i < n; i++) {
+            if (ans < dL[i]) {
+                ans = dL[i];
+            }
+        }
+        for (int i = 1; i < n - 1; i++) {
+            if (ans < dL[i - 1] + dR[i + 1]) {
+                ans = dL[i - 1] + dR[i + 1];
+            }
+        }
+        System.out.println(ans);
+    }
+}
