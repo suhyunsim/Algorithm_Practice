76fb11869282f49cffb3777c7d7046293f763d45
suhyunsim <cnc1106@naver.com>
2021-08-09 02:15:51 +0900

    BOJ: 13549 풀이
    - Queue 두개로 풀이
    
    Issue #156

---
+++ src/main/java/com/poogle/BOJ/Q13549/Main.java
@@ -0,0 +1,44 @@
+package main.java.com.poogle.BOJ.Q13549;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static final int MAX = 1000000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int k = sc.nextInt();
+        boolean[] check = new boolean[MAX];
+        int[] dist = new int[MAX];
+        check[n] = true;
+        dist[n] = 0;
+        Queue<Integer> nowQueue = new LinkedList<>();
+        Queue<Integer> nextQueue = new LinkedList<>();
+        nowQueue.add(n);
+        while (!nowQueue.isEmpty()) {
+            int now = nowQueue.remove();
+            for (int next : new int[]{now * 2, now - 1, now + 1}) {
+                if (next >= 0 && next < MAX) {
+                    if (!check[next]) {
+                        check[next] = true;
+                        if (now * 2 == next) {
+                            nowQueue.add(next);
+                            dist[next] = dist[now];
+                        } else {
+                            nextQueue.add(next);
+                            dist[next] = dist[now] + 1;
+                        }
+                    }
+                }
+            }
+            if (nowQueue.isEmpty()) {
+                nowQueue = nextQueue;
+                nextQueue = new LinkedList<>();
+            }
+        }
+        System.out.println(dist[k]);
+    }
+}
