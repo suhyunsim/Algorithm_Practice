c652e4b1e471a3beff049342bc15cae6eab5a714
suhyunsim <cnc1106@naver.com>
2021-08-09 03:06:50 +0900

    BOJ: 1261 풀이
    Issue #157

---
+++ src/main/java/com/poogle/BOJ/Q1261/Main.java
@@ -0,0 +1,67 @@
+package main.java.com.poogle.BOJ.Q1261;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        sc.nextLine();
+        int[][] a = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                a[i][j] = s.charAt(j) - '0';
+            }
+        }
+        int[] dx = {0, 0, -1, 1};
+        int[] dy = {-1, 1, 0, 0};
+        int[][] dist = new int[n][m];
+        Queue<Place> nowQueue = new LinkedList<>();
+        Queue<Place> nextQueue = new LinkedList<>();
+        nowQueue.offer(new Place(0, 0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dist[i][j] = -1;
+            }
+        }
+        dist[0][0] = 0;
+        while (!nowQueue.isEmpty()) {
+            Place p = nowQueue.remove();
+            int x = p.x;
+            int y = p.y;
+            for (int k = 0; k < 4; k++) {
+                int nx = x + dx[k];
+                int ny = y + dy[k];
+                if (0 <= nx && nx < n && 0 <= ny && ny < m) {
+                    if (dist[nx][ny] == -1) {
+                        if (a[nx][ny] == 0) {
+                            dist[nx][ny] = dist[x][y];
+                            nowQueue.offer(new Place(nx, ny));
+                        } else {
+                            dist[nx][ny] = dist[x][y] + 1;
+                            nextQueue.offer(new Place(nx, ny));
+                        }
+                    }
+                }
+            }
+            if (nowQueue.isEmpty()) {
+                nowQueue = nextQueue;
+                nextQueue = new LinkedList<>();
+            }
+        }
+        System.out.println(dist[n - 1][m - 1]);
+    }
+}
+
+class Place {
+    int x, y;
+
+    public Place(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
