2b103dde8311f3859701021a9e1a287e6a05158d
suhyunsim <cnc1106@naver.com>
2021-08-09 02:27:09 +0900

    BOJ: 13549 풀이
    Closed #156

--- src/main/java/com/poogle/BOJ/Q13549/Main.java
+++ src/main/java/com/poogle/BOJ/Q13549/Main.java
@@ -1,5 +1,6 @@
 package main.java.com.poogle.BOJ.Q13549;
 
+import java.util.ArrayDeque;
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Scanner;
@@ -15,28 +16,50 @@ public class Main {
         int[] dist = new int[MAX];
         check[n] = true;
         dist[n] = 0;
-        Queue<Integer> nowQueue = new LinkedList<>();
-        Queue<Integer> nextQueue = new LinkedList<>();
-        nowQueue.add(n);
-        while (!nowQueue.isEmpty()) {
-            int now = nowQueue.remove();
+        //큐 두개로 풀이
+//        Queue<Integer> nowQueue = new LinkedList<>();
+//        Queue<Integer> nextQueue = new LinkedList<>();
+//        nowQueue.add(n);
+//        while (!nowQueue.isEmpty()) {
+//            int now = nowQueue.remove();
+//            for (int next : new int[]{now * 2, now - 1, now + 1}) {
+//                if (next >= 0 && next < MAX) {
+//                    if (!check[next]) {
+//                        check[next] = true;
+//                        if (now * 2 == next) {
+//                            nowQueue.add(next);
+//                            dist[next] = dist[now];
+//                        } else {
+//                            nextQueue.add(next);
+//                            dist[next] = dist[now] + 1;
+//                        }
+//                    }
+//                }
+//            }
+//            if (nowQueue.isEmpty()) {
+//                nowQueue = nextQueue;
+//                nextQueue = new LinkedList<>();
+//            }
+//        }
+//        System.out.println(dist[k]);
+        // 덱으로 풀이
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        deque.add(n);
+        while (!deque.isEmpty()) {
+            int now = deque.poll();
             for (int next : new int[]{now * 2, now - 1, now + 1}) {
                 if (next >= 0 && next < MAX) {
                     if (!check[next]) {
                         check[next] = true;
                         if (now * 2 == next) {
-                            nowQueue.add(next);
+                            deque.addFirst(next);
                             dist[next] = dist[now];
                         } else {
-                            nextQueue.add(next);
+                            deque.addLast(next);
                             dist[next] = dist[now] + 1;
                         }
                     }
                 }
-            }
-            if (nowQueue.isEmpty()) {
-                nowQueue = nextQueue;
-                nextQueue = new LinkedList<>();
             }
         }
         System.out.println(dist[k]);
