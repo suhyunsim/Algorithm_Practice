bb48b3aa900bbaa51beca527d9812c8b92f3c61f
suhyunsim <cnc1106@naver.com>
2022-03-06 23:08:05 +0900

    주석 추가
    Issue #219

--- src/main/java/com/poogle/BOJ/Q11286/Main.java
+++ src/main/java/com/poogle/BOJ/Q11286/Main.java
@@ -14,6 +14,11 @@ public class Main {
                 else return -1;
             }
         });
+        /* 같은 Comparable인데 다르게 표현할 수도 있음, 절댓값이 같으면 실제 숫자 기준으로, 아니라면 절댓값을 기준으로 오름차순 정렬
+        PriorityQueue<Integer> queue = new PriorityQueue<>(((o1, o2) ->
+        Math.abs(o1) == Math.abs(o2) ? Integer.compare(o1, o2) : Integer.compare(Math.abs(o1), Math.abs(o2))
+        ));
+        */
         for (int i = 0; i < n; i++) {
             int num = sc.nextInt();
             if (num == 0) {
