431aa4f9ea0d33a7ef051164948393ae204abb6c
suhyunsim <cnc1106@naver.com>
2022-03-10 15:04:33 +0900

    문제풀이 정리

---
+++ src/main/java/com/poogle/PGTest/kakaoEnterp/Q1/Main.java
@@ -0,0 +1,30 @@
+package main.java.com.poogle.PGTest.kakaoEnterp.Q1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int cnt = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        String[] sizes = input.split(" ");
+        int n = Integer.parseInt(sizes[0]);
+        int m = Integer.parseInt(sizes[1]);
+        calculate(n, m);
+        System.out.println(cnt);
+    }
+
+    private static void calculate(int n, int m) {
+        if (n <= 1 || m <= 1) {
+            cnt += n * m;
+            return;
+        }
+        cnt += n * m;
+        cnt -= (n - 2) * (m - 2);
+        n -= 2;
+        m -= 2;
+        calculate(n, m);
+    }
+}
