c11d97a23784255ae47468a9dd9f55d0a9d5a90a
suhyunsim <cnc1106@naver.com>
2022-03-12 15:58:53 +0900

    BOJ: 1920 풀이
    Closed #223

---
+++ src/main/java/com/poogle/BOJ/Q1920/Main.java
@@ -0,0 +1,45 @@
+package main.java.com.poogle.BOJ.Q1920;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    static int[] arr;
+    static int m;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        m = sc.nextInt();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            if (binarySearch(sc.nextInt()) >= 0) {
+                sb.append(1).append('\n');
+            } else {
+                sb.append(0).append('\n');
+            }
+        }
+        System.out.println(sb);
+    }
+
+    private static int binarySearch(int target) {
+        int start = 0;
+        int end = arr.length - 1;
+        while (start <= end) {
+            int mid = (start + end) / 2;
+            if (arr[mid] < target) {
+                start = mid + 1;
+            } else if (arr[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
