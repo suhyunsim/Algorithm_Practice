c24fe851136c91afb796f9475cd3468983149502
suhyunsim <cnc1106@naver.com>
2022-03-14 15:55:21 +0900

    코드 포맷팅

--- src/main/java/com/poogle/BOJ/Q10816/Main.java
+++ src/main/java/com/poogle/BOJ/Q10816/Main.java
@@ -1,17 +1,19 @@
 package main.java.com.poogle.BOJ.Q10816;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n;
-    static int [] arr;
+    static int[] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         arr = new int[n];
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
