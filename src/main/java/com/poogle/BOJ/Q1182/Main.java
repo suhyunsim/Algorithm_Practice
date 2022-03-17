d6795cf4483073dbd8a6ba5defef782f13bb9fb1
suhyunsim <cnc1106@naver.com>
2022-03-17 16:04:45 +0900

    코드 포맷팅

--- src/main/java/com/poogle/BOJ/Q1182/Main.java
+++ src/main/java/com/poogle/BOJ/Q1182/Main.java
@@ -3,24 +3,30 @@ package main.java.com.poogle.BOJ.Q1182;
 import java.util.Scanner;
 
 public class Main {
+    static int[] a;
+    static int s;
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        int s = sc.nextInt();
-        int[] a = new int[n];
+        s = sc.nextInt();
+        a = new int[n];
         for (int i = 0; i < n; i++) {
             a[i] = sc.nextInt();
         }
-        int ans = go(a, s, 0, 0);
-        if (s == 0) ans -= 1; //크기가 양수인 부분수열
+        int ans = go(0, 0);
+        if (s == 0)
+            ans -= 1; //크기가 양수인 부분수열
         System.out.println(ans);
     }
 
-    private static int go(int[] a, int s, int i, int sum) {
+    private static int go(int i, int sum) {
         if (i == a.length) {
-            if (sum == s) return 1;
-            else return 0;
+            if (sum == s)
+                return 1;
+            else
+                return 0;
         }
-        return go(a, s, i + 1, sum + a[i]) + go(a, s, i + 1, sum);
+        return go(i + 1, sum + a[i]) + go(i + 1, sum);
     }
 }
