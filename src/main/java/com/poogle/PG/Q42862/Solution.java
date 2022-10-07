373ebebf560dd7d940fc754972fd7ef7b69988ab
suhyunsim <cnc1106@naver.com>
2022-10-07 16:21:42 +0900

    PG: 42862 테스트 케이스 추가

--- src/main/java/com/poogle/PG/Q42862/Solution.java
+++ src/main/java/com/poogle/PG/Q42862/Solution.java
@@ -26,6 +26,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-        System.out.println(solution(3, new int[]{1, 2}, new int[]{1}));
+        System.out.println(solution(5, new int[]{2, 4}, new int[]{1, 3, 5}));
+        System.out.println(solution(5, new int[]{2, 4}, new int[]{3}));
+        System.out.println(solution(3, new int[]{3}, new int[]{1}));
     }
 }
