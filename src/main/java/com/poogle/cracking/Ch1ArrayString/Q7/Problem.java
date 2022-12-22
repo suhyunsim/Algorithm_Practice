f2b510f809f7fa7f30ba9ce39207e0f14522e050
suhyunsim <cnc1106@naver.com>
2022-12-22 18:57:56 +0900

    CtCI: 1.7 Rotate Matrix
    Issue #274

---
+++ src/main/java/com/poogle/cracking/Ch1ArrayString/Q7/Problem.java
@@ -0,0 +1,70 @@
+package main.java.com.poogle.cracking.Ch1ArrayString.Q7;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Problem {
+    /*
+    left -> top
+    bottom -> left
+    right -> bottom
+    top -> right
+     */
+    public static void main(String[] args) {
+        int[][] matrix = randomMatrix(10, 10, 0, 9);
+        printMatrix(matrix);
+        rotate(matrix);
+        System.out.println();
+        printMatrix(matrix);
+    }
+
+    public static boolean rotate(int[][] matrix) {
+        if (matrix.length == 0 || matrix.length != matrix[0].length)
+            return false;
+        int n = matrix.length;
+        for (int layer = 0; layer < n / 2; layer++) {
+            int first = layer;
+            int last = n - 1 - layer;
+            for (int i = first; i < last; i++) {
+                int offset = i - first;
+                int top = matrix[first][i];
+                // left -> top
+                matrix[first][i] = matrix[last - offset][first];
+                // bottom -> left
+                matrix[last - offset][first] = matrix[last][last - offset];
+                // right -> bottom
+                matrix[last][last - offset] = matrix[i][last];
+                // top -> right
+                matrix[i][last] = top;
+            }
+        }
+        return true;
+    }
+
+    public static int[][] randomMatrix(int M, int N, int min, int max) {
+        int[][] matrix = new int[M][N];
+        for (int i = 0; i < M; i++) {
+            for (int j = 0; j < N; j++) {
+                matrix[i][j] = ThreadLocalRandom.current().nextInt(min, max);
+            }
+        }
+        return matrix;
+    }
+
+    public static void printMatrix(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (matrix[i][j] < 10 && matrix[i][j] > -10) {
+                    System.out.print(" ");
+                }
+                if (matrix[i][j] < 100 && matrix[i][j] > -100) {
+                    System.out.print(" ");
+                }
+                if (matrix[i][j] >= 0) {
+                    System.out.print(" ");
+                }
+                System.out.print(" " + matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
