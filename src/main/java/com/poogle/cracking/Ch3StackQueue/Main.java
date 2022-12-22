257f950a5f51edd67dbda47be12f7bf54d46f73a
suhyunsim <cnc1106@naver.com>
2022-12-22 13:27:39 +0900

    CtCI: 3.4 Queues via Stacks
    Issue #276

---
+++ src/main/java/com/poogle/cracking/Ch3StackQueue/Main.java
@@ -0,0 +1,37 @@
+package main.java.com.poogle.cracking.Ch3StackQueue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class Main {
+    public static void main(String[] args) {
+        MyQueue<Integer> myQueue = new MyQueue<>();
+        Queue<Integer> testQueue = new LinkedList<>();
+
+        for (int i = 0; i < 100; i++) {
+            int choice = ThreadLocalRandom.current().nextInt(1, 11);
+            if (choice <= 5) {
+                int element = ThreadLocalRandom.current().nextInt(1, 11);
+                testQueue.add(element);
+                myQueue.add(element);
+                System.out.println("Enqueued: " + element);
+            } else if (testQueue.size() > 0) {
+                int top1 = testQueue.remove();
+                int top2 = myQueue.remove();
+                if (top1 != top2) {
+                    System.out.println("Fail - Different Tops: " + top1 + ", " + top2);
+                }
+                System.out.println("Dequeued: " + top1);
+            }
+
+            if (testQueue.size() == myQueue.size()) {
+                if (testQueue.size() > 0 && testQueue.peek() != myQueue.peek()) {
+                    System.out.println("Fail - Different Tops: " + testQueue.peek() + ", " + myQueue.peek());
+                }
+            } else {
+                System.out.println("Fail - Different Sizes");
+            }
+        }
+    }
+}
---
+++ src/main/java/com/poogle/cracking/Ch3StackQueue/MyQueue.java
@@ -0,0 +1,44 @@
+package main.java.com.poogle.cracking.Ch3StackQueue;
+
+import java.util.Stack;
+
+public class MyQueue<T> {
+    /*
+    stackNewest: new -> top (inBox) add
+    stackOldest: last -> top (outBox) peek, poll
+     */
+    Stack<T> stackNewest, stackOldest;
+
+    public MyQueue() {
+        stackNewest = new Stack<>();
+        stackOldest = new Stack<>();
+    }
+
+    public int size() {
+        return stackNewest.size() + stackOldest.size();
+    }
+
+    public void add(T value) {
+        // new -> top
+        stackNewest.push(value);
+    }
+
+    // move newest -> oldest
+    private void shiftStacks() {
+        if (stackOldest.isEmpty()) {
+            while (!stackNewest.isEmpty()) {
+                stackOldest.push(stackNewest.pop());
+            }
+        }
+    }
+
+    public T peek() {
+        shiftStacks();
+        return stackOldest.peek();
+    }
+
+    public T remove() {
+        shiftStacks();
+        return stackOldest.pop();
+    }
+}
