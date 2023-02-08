f4ffd34d41caf75dd5f7212c1cddba1d918665be
suhyunsim <cnc1106@naver.com>
2023-02-08 19:06:00 +0900

    BOJ: 1074 풀이 수정
    Issue #197

--- src/main/java/com/poogle/BOJ/Q1074/Main.java
+++ src/main/java/com/poogle/BOJ/Q1074/Main.java
@@ -1,22 +1,33 @@
 package main.java.com.poogle.BOJ.Q1074;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int r = sc.nextInt();
-        int c = sc.nextInt();
-        System.out.println(calculate(n, r, c));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = stringToInt(st.nextToken());
+        int r = stringToInt(st.nextToken());
+        int c = stringToInt(st.nextToken());
+
+        bw.write(String.valueOf(calculate(N, r, c)));
+        bw.flush();
+        bw.close();
     }
 
-    private static int calculate(int n, int r, int c) {
-        if (n == 0) return 0;
-        int half = 1 << n - 1;
-        if (r < half && c < half) return calculate(n - 1, r, c);
-        if (r < half && c >= half) return half * half + calculate(n - 1, r, c + half);
-        if (r >= half && c < half) return 2 * half * half + calculate(n - 1, r + half, c);
-        return 3 * half * half + calculate(n - 1, r + half, c + half);
+    private static int calculate(int N, int r, int c) {
+        if (N == 0) return 0;
+        int half = 1 << N - 1;
+        if (r < half && c < half) return calculate(N - 1, r, c);
+        if (r < half && c >= half) return half * half + calculate(N - 1, r, c - half);
+        if (r >= half && c < half) return half * half * 2 + calculate(N - 1, r - half, c);
+        return half * half * 3 + calculate(N-1, r - half, c - half);
+    }
+
+    public static int stringToInt(String st) {
+        return Integer.parseInt(st);
     }
 }
