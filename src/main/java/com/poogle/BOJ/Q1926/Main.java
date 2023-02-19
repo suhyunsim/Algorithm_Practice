583ae0723ad2c306b7e82182f4d3b677ed8bb9d2
suhyunsim <cnc1106@naver.com>
2023-02-20 08:57:37 +0900

    BOJ: 1926 풀이 수정
    Closed #120

--- src/main/java/com/poogle/BOJ/Q1926/Main.java
+++ src/main/java/com/poogle/BOJ/Q1926/Main.java
@@ -1,62 +1,75 @@
 package main.java.com.poogle.BOJ.Q1926;
 
+import java.io.*;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
+    static int cnt = 0;
+    static int max = 0;
     static int n;
     static int m;
-    static int paintCnt = 0;
-    static int paintSize = 0;
-    static boolean[][] check;
-    static int[][] board;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        board = new int[n][m];
+    static int[][] picture;
+
+    static boolean[][] visited;
+    static int[] dx = new int[]{-1, 0, 1, 0};
+    static int[] dy = new int[]{0, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        picture = new int[n][m];
+        visited = new boolean[n][m];
         for (int i = 0; i < n; i++) {
+            String[] row = br.readLine().split(" ");
             for (int j = 0; j < m; j++) {
-                board[i][j] = sc.nextInt();
+                picture[i][j] = Integer.parseInt(row[j]);
             }
         }
-        check = new boolean[n][m];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (board[i][j] == 1 && !check[i][j]) {
+                if (picture[i][j] == 1 && !visited[i][j]) {
+                    visited[i][j] = true;
                     bfs(i, j);
+                    cnt++;
                 }
             }
         }
-        System.out.println(paintCnt);
-        System.out.println(paintSize);
+        sb.append(cnt).append("\n").append(max);
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void bfs(int x, int y) {
+    private static void bfs(int i, int j) {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(x, y));
-        int size = 0;
+        queue.offer(new Node(i, j));
+        int size = 1;
         while (!queue.isEmpty()) {
             Node node = queue.poll();
-            size++;
-            for (int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (check[nx][ny] || board[nx][ny] == 0) continue;
-                check[nx][ny] = true;
-                queue.add(new Node(nx, ny));
+            for (int k = 0; k < 4; k++) {
+                int nx = node.x + dx[k];
+                int ny = node.y + dy[k];
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+                    continue;
+                if (picture[nx][ny] == 0 || visited[nx][ny])
+                    continue;
+                queue.offer(new Node(nx, ny));
+                visited[nx][ny] = true;
+                size++;
             }
         }
-        paintCnt++;
-        if (size > 1) size--;
-        paintSize = Math.max(paintSize, size);
+        max = Math.max(size, max);
     }
 }
+
 
 class Node {
     int x;
