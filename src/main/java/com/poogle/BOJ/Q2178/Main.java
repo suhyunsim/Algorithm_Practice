62d1ca631506934e643aee3d4432e8b24530812f
suhyunsim <cnc1106@naver.com>
2023-02-20 22:18:30 +0900

    BOJ: 2178 풀이 수정
    Issue #39

--- src/main/java/com/poogle/BOJ/Q2178/Main.java
+++ src/main/java/com/poogle/BOJ/Q2178/Main.java
@@ -1,59 +1,65 @@
 package main.java.com.poogle.BOJ.Q2178;
 
-import java.util.*;
-import java.util.stream.Collectors;
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
     static int[][] board;
     static int[][] dist;
-    static int[] dx = {1, 0, -1, 0};
-    static int[] dy = {0, 1, 0, -1};
-    static int cnt = 0;
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        m = sc.nextInt();
-        board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            List<Integer> list = Arrays.stream(sc.next().split("")).map(Integer::new).collect(Collectors.toList());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = list.get(j);
-            }
-        }
-        dist = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
+    static int N;
+    static int M;
+
+    static int[] dx = new int[]{1, 0, -1, 0};
+    static int[] dy = new int[]{0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        board = new int[N][M];
+        dist = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            String[] row = br.readLine().split("");
+            for (int j = 0; j < M; j++) {
+                board[i][j] = Integer.parseInt(row[j]);
                 dist[i][j] = -1;
             }
         }
         dist[0][0] = 0;
-        Node first = new Node(0, 0);
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (board[i][j] == 1 && dist[i][j] < 0) {
-                    bfs(first);
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (dist[i][j] < 0 && board[i][j] == 1) {
+                    bfs(new Node(0, 0));
                 }
             }
         }
-        cnt = dist[n - 1][m - 1] + 1;
-        System.out.println(cnt);
+        bw.write(String.valueOf(dist[N - 1][M - 1] + 1));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void bfs(Node first) {
+    private static void bfs(Node firstNode) {
         Queue<Node> queue = new LinkedList<>();
-        queue.add(first);
+        queue.offer(firstNode);
         while (!queue.isEmpty()) {
             Node node = queue.poll();
-            for (int i = 0; i < 4; i++) {
-                int nx = node.x + dx[i];
-                int ny = node.y + dy[i];
-                if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
-                if (dist[nx][ny] >= 0 || board[nx][ny] == 0) continue;
+            for (int k = 0; k < 4; k++) {
+                int nx = node.x + dx[k];
+                int ny = node.y + dy[k];
+                if (nx < 0 || nx >= N || ny < 0 || ny >= M)
+                    continue;
+                if (dist[nx][ny] > -1 || board[nx][ny] == 0)
+                    continue;
                 dist[nx][ny] = dist[node.x][node.y] + 1;
-                queue.add(new Node(nx, ny));
+                queue.offer(new Node(nx, ny));
             }
         }
     }
