e8c8355edc7297fe38790aa231bf4140053eabc0
suhyunsim <cnc1106@naver.com>
2023-03-07 15:54:27 +0900

    BOJ: 1149 풀이 수정
    Issue #136

--- src/main/java/com/poogle/BOJ/Q1149/Main.java
+++ src/main/java/com/poogle/BOJ/Q1149/Main.java
@@ -1,26 +1,32 @@
 package main.java.com.poogle.BOJ.Q1149;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] minCost;
-    static int[][] cost;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        int[][] cost = new int[N + 1][3];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        minCost = new int[n + 1][3];
-        cost = new int[n + 1][3];
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < 3; j++) {
-                cost[i][j] = sc.nextInt();
+                cost[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 1; i <= n; i++) {
+
+        int[][] minCost = new int[N + 1][3];
+        for (int i = 1; i <= N; i++) {
             minCost[i][0] = Math.min(minCost[i - 1][1], minCost[i - 1][2]) + cost[i][0];
             minCost[i][1] = Math.min(minCost[i - 1][0], minCost[i - 1][2]) + cost[i][1];
             minCost[i][2] = Math.min(minCost[i - 1][0], minCost[i - 1][1]) + cost[i][2];
         }
-        System.out.println(Math.min(Math.min(minCost[n][0], minCost[n][1]), minCost[n][2]));
+        bw.write(String.valueOf(Math.min(Math.min(minCost[N][0], minCost[N][1]), minCost[N][2])));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 }
