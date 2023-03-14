161193fc4d4493bd7fed2800f32dc427bb308c9a
suhyunsim <cnc1106@naver.com>
2023-03-14 16:58:42 +0900

    BOJ: 2606 풀이
    Issue #236

--- src/main/java/com/poogle/BOJ/Q2606/Main.java
+++ src/main/java/com/poogle/BOJ/Q2606/Main.java
@@ -1,37 +1,50 @@
 package main.java.com.poogle.BOJ.Q2606;
 
+import java.io.*;
 import java.util.ArrayList;
-import java.util.Scanner;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int n, e, result;
-    static ArrayList<Integer>[] graph = new ArrayList[101];
-    static boolean[] visited = new boolean[101];
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n = sc.nextInt();
-        e = sc.nextInt();
+    static int cnt = 0;
+    static boolean[] visited = new boolean[101];
+    static List<Integer>[] graph = new ArrayList[101];
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+        int l = Integer.parseInt(br.readLine());
+
         for (int i = 1; i <= n; i++) {
             graph[i] = new ArrayList<>();
         }
-        for (int i = 0; i < e; i++) {
-            int x = sc.nextInt();
-            int y = sc.nextInt();
+
+        StringTokenizer st;
+        for (int i = 0; i < l; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
             graph[x].add(y);
             graph[y].add(x);
         }
+
         dfs(1);
-        System.out.println(result - 1);
+
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+        br.close();
     }
 
-    private static void dfs(int x) {
-        result++;
-        visited[x] = true;
-        for (int i = 0; i < graph[x].size(); i++) {
-            int y = graph[x].get(i);
-            if (!visited[y]) {
-                dfs(y);
+    private static void dfs(int start) {
+        cnt++;
+        visited[start] = true;
+        for (int i = 0; i < graph[start].size(); i++) {
+            int num = graph[start].get(i);
+            if (!visited[num]) {
+                dfs(num);
             }
         }
     }
