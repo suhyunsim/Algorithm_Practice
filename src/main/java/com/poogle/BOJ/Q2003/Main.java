ae07c388fcef880edc5a353cca844988bd9bc20e
suhyunsim <cnc1106@naver.com>
2023-03-16 19:23:38 +0900

    BOJ: 2003 풀이
    Closed #285

---
+++ src/main/java/com/poogle/BOJ/Q2003/Main.java
@@ -0,0 +1,48 @@
+package main.java.com.poogle.BOJ.Q2003;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int cnt, N, M;
+    static int[] arr;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        twoPointer();
+        bw.write(String.valueOf(cnt));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    static void twoPointer() {
+        int start = 0, end = 0, sum = 0;
+
+        while (true) {
+            if (sum >= M) {
+                sum -= arr[start++];
+            } else if (end >= arr.length) {
+                break;
+            } else {
+                sum += arr[end++];
+            }
+            if (sum == M)
+                cnt++;
+        }
+
+    }
+}
