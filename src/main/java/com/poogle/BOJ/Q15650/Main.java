93c28de69c9e18c434b9dde303fdf8c1e59d3a34
suhyunsim <cnc1106@naver.com>
2023-03-23 23:41:43 +0900

    BOJ: 15650 풀이 수정
    Issue #56

--- src/main/java/com/poogle/BOJ/Q15650/Main.java
+++ src/main/java/com/poogle/BOJ/Q15650/Main.java
@@ -1,10 +1,56 @@
 package main.java.com.poogle.BOJ.Q15650;
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static int[] a = new int[10];
-/* 순서로 풀이
+
+    static int M;
+    static int N;
+
+    static int[] result = new int[9];
+
+    static StringBuilder sb = new StringBuilder();
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        /* 순서
+        go(0, 1, n, m);
+         */
+
+        //선택
+        go(1, 0);
+        bw.write(String.valueOf(sb));
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    //선택으로 풀이
+    private static void go(int index, int cnt) {
+        if (cnt == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(result[i]).append(" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        if (index > N)
+            return;
+        result[cnt] = index;
+        go(index + 1, cnt + 1);
+        result[cnt] = 0;
+        go(index + 1, cnt);
+    }
+
+    /* 순서로 풀이
     //index번 째에 올 수 있는 수의 범위의 시작: start
     static void go(int index, int start, int n, int m) {
         if (index == m) {
@@ -20,32 +66,5 @@ public class Main {
             go(index + 1, i + 1, n, m);
         }
     }
-*/
-    //선택으로 풀이
-    static void go(int index, int selected, int n, int m) {
-        if (selected == m) {
-            for (int i = 0; i < m; i++) {
-                System.out.print(a[i]);
-                if (i != m - 1) System.out.print(' ');
-            }
-            System.out.println();
-            return;
-        }
-        if (index > n) return;
-        //선택했을 때
-        a[selected] = index;
-        go(index + 1, selected + 1, n, m);
-        //선택하지 않았을 때
-        a[selected] = 0;
-        go(index + 1, selected, n, m);
-    }
-
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-//순서        go(0, 1, n, m);
-        //선택
-        go(1, 0, n, m);
-    }
+    */
 }
