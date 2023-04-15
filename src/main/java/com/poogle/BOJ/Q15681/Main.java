611c3aff8e0f816c2682c125b22ab00aa06bf746
suhyunsim <cnc1106@naver.com>
2023-04-16 01:45:00 +0900

    BOJ: 15681 풀이
    Closed #295

---
+++ src/main/java/com/poogle/BOJ/Q15681/Main.java
@@ -0,0 +1,52 @@
+package main.java.com.poogle.BOJ.Q15681;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int[] childArr;
+    static List<Integer>[] list;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int q = Integer.parseInt(st.nextToken());
+        list = new ArrayList[n + 1];
+        for (int i = 1; i < n + 1; i++) {
+            list[i] = new ArrayList<>();
+        }
+        childArr = new int[n + 1];
+        Arrays.fill(childArr, 1);
+        for (int i = 0; i < n - 1; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            list[u].add(v);
+            list[v].add(u);
+        }
+        postorder(r, -1);
+        for (int i = 0; i < q; i++) {
+            int index = Integer.parseInt(br.readLine());
+            System.out.println(childArr[index]);
+        }
+    }
+
+    private static void postorder(int index, int parent) {
+        for (int next : list[index]) {
+            if (parent != next) {
+                postorder(next, index);
+            }
+        }
+        if (parent != -1) {
+            childArr[parent] += childArr[index];
+        }
+    }
+}
